49630d08e20b837e0aabed16fa8ba38250915c27
Max Hirata <max.hirata118@gmail.com>
2021-01-20 22:29:14 -0800

    bubble sort animation

---
+++ src/components/SortingAlgorithm/BubbleSort.js
@@ -0,0 +1,31 @@
+export default function bubblesort(array) {
+    const animation = [];
+    if(array.length <= 1) return array;
+    _bubblesort(array, animation);
+
+    return animation;
+}
+
+function _bubblesort(array, animation) {
+    let swapped = true;
+    while (swapped) {
+        swapped = false;
+        for (let i = 1; i < array.length; i++) {
+            if(array[i] < array[i - 1]) {
+                swapped = true;
+
+                animation.push([i , i - 1]);
+                animation.push([i , i - 1]);
+                animation.push([i - 1, array[i]]);
+                animation.push([i, i - 1]);
+                animation.push([i, i - 1 ]);
+                animation.push([i, array[i - 1]]);
+
+                //swap values
+                let temp = array[i];
+                array[i] = array[i-1];
+                array[i-1] = temp;
+            }
+        }
+    }
+}
